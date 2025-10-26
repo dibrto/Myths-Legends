0f7b5790a79dc90bf71db792e6cdada16f4bba8e
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-10-26 09:14:35 +0200

    Fix user model

--- src/models/User.js
+++ src/models/User.js
@@ -6,14 +6,13 @@ const userSchema = new Schema({
     email: {
         type: String
         , required: [true, "Email is required"]
-        // , unique: [true, "Email already exist"]
-        // , minLenght: [10, "Email need to be at least 10 characters long"]
+        , minLenght: [10, "Email need to be at least 10 characters long"]
     }
 
     , password: {
         type: String
         , required: [true, "Password is required"]
-        // , minLenght: [4, "Password need to be at least 4 characters long"]
+        , minLenght: [4, "Password need to be at least 4 characters long"]
     }
 });
 
