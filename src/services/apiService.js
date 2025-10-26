d3620c12c816a1c4c5d14ba66749dd8a2838495e
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-10-26 11:49:38 +0200

    Implement api getLastThreeMyths

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,3 +1,18 @@
+import Myth from "../models/Myth.js";
+
 export async function getLastThreeMyths(){
-    return { test: 1 };
+    return Myth.find()
+        .sort({ createdAt: -1 })
+        .limit(3)
+        .select({
+          name: 1
+          , origin: 1
+          , role: 1
+          , symbol: 1
+          , era: 1
+          , image: 1
+          , owner: 1
+          , createdAt: 1
+        })
+        .lean();
 }
