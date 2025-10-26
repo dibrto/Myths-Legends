7fe20a60003c194f17eaada9f469198b43ac3000
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-10-26 12:05:23 +0200

    Fix select

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -4,15 +4,6 @@ export async function getLastThreeMyths(){
     return Myth.find()
         .sort({ createdAt: -1 })
         .limit(3)
-        .select({
-          name: 1
-          , origin: 1
-          , role: 1
-          , symbol: 1
-          , era: 1
-          , image: 1
-          , owner: 1
-          , createdAt: 1
-        })
+        .select("name origin role symbol era image owner createdAt")
         .populate("owner", "email");
 }
