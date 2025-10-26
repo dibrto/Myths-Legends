8ea1d1a7c6bacb3fef7f436f8abe361263feb677
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-10-26 11:38:06 +0200

    Imeplment fetch data from api endpoint

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { mythService } from "../services/index.js";
+import { getErrMsg } from "../utils/errorUtil.js";
 
 const homeController = Router();
 
@@ -14,7 +15,16 @@ homeController.get("/dashboard", async (req, res) => {
 });
 
 homeController.get("/report", async (req, res) => {
-    res.render("report");
+    try { 
+        const response = await fetch("http://localhost:3000/api/report/myths/latest");
+        const data = await response.json();
+        console.log(data);        
+        res.render("report", { myth: data});
+    }
+    catch (err) {
+        const errMsg = getErrMsg(err);
+        res.render("report", {errMsg, myth: null});
+    }
 });
 
 export default homeController;
