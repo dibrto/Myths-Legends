419507d01c1ffbab75df8e1c6b5f76d63d6f64d0
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-10-26 09:19:38 +0200

    Render home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,8 +4,7 @@ const homeController = Router();
 
 // TODO: fix home
 homeController.get("/", async (req, res) => {
-    res.send("home");
-    // res.render("home");
+    res.render("home");
 });
 
 export default homeController;
